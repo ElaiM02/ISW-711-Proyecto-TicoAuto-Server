56d509aadab661cf83dbc055c3e45f0b5ddb0c85
Eliam Araya <eliamaa02@gmail.com>
2026-03-02 18:04:37 -0600

    Se crea el index en el cual se crea la conexion a la base de datos, se prueba si funciona la conexion y todo OK

---
+++ index.js
@@ -0,0 +1,29 @@
+require('dotenv').config();
+const dns = require('dns');
+dns.setDefaultResultOrder('ipv4first');
+dns.setServers(['8.8.8.8', '8.8.4.4']);
+
+const express = require('express');
+const cors = require("cors");
+const bodyParser = require("body-parser");
+const mongoose = require('mongoose');
+
+const mongoString = process.env.DATABASE_URL;
+
+mongoose.connect(mongoString);
+const database = mongoose.connection;
+
+
+database.on('error', (error) => {
+    console.log(error)
+});
+
+database.once('connected', () => {
+    console.log('Database Connected');
+});
+
+
+const app = express();
+
+//start the app
+app.listen(3008, () => console.log(`UTN API service listening on port 3008!`))
