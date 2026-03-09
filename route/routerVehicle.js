de945e8f687f56a1116c5ed54ae3a1be0e77acac
koala31BM <kenneger31@gmail.com>
2026-03-08 21:59:59 -0600

    Se da actualizacion de las rutas para auto para su funcionamiento

--- route/routerVehicle.js
+++ route/routerVehicle.js
@@ -1,8 +1,17 @@
 const express = require('express');
 const router = express.Router();
-const vehicleController = require('../controllers/vehicleController');
+const vehicleController = require('../controller/controllerVehicle');
 
+// CREAR VEHICULO
+router.post('/', vehicleController.createVehicle);
+
+// OBTENER VEHICULOS
+router.get('/', vehicleController.getVehicles);
+
+// ACTUALIZAR
 router.put('/:id', vehicleController.updateVehicle);
+
+// ELIMINAR
 router.delete('/:id', vehicleController.deleteVehicle);
 
 module.exports = router;
