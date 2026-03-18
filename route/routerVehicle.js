335f68dfcae0ac8abdcef8c8c84c669ad7fd7e43
koala31BM <kenneger31@gmail.com>
2026-03-17 18:57:44 -0600

    se actualiza rutas y se implementa upload para la creacion de la imagen

--- route/routerVehicle.js
+++ route/routerVehicle.js
@@ -1,14 +1,17 @@
 const express = require('express');
 const router = express.Router();
+
 const { authenticateToken } = require('../controller/controllerAuth');
 const vehicleController = require('../controller/controllerVehicle');
+const upload = require('../middleware/upload');
 
 
+// RUTAS
 router.get('/vehicle', vehicleController.getVehicles);
 
-router.get('/vehicle/:id',authenticateToken, vehicleController.getVehicleById);
-router.post('/vehicle', authenticateToken, vehicleController.createVehicle);
+router.get('/vehicle/:id', authenticateToken, vehicleController.getVehicleById);
+
 router.patch('/vehicle/:id', authenticateToken, vehicleController.updateVehicle);
 router.delete('/vehicle/:id', authenticateToken, vehicleController.deleteVehicle);
-
+router.post('/vehicle', upload.single('image'), vehicleController.createVehicle);
 module.exports = router;
