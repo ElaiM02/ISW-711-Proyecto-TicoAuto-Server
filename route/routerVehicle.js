4ec222682c99f5b164c1e384d1b04c5662e8ce62
Eliam Araya <eliamaa02@gmail.com>
2026-03-18 00:10:31 -0600

    Se agregan las rutas de question y answer
    B

--- route/routerVehicle.js
+++ route/routerVehicle.js
@@ -3,15 +3,25 @@ const router = express.Router();
 
 const { authenticateToken } = require('../controller/controllerAuth');
 const vehicleController = require('../controller/controllerVehicle');
+const controllerQuestion = require('../controller/controllerQuestion');
+const controllerAnswer = require('../controller/controllerAnswer');
+
 const upload = require('../middleware/upload');
 
+const controllerAnswer = require('../controller/controllerAnswer');
+const controllerQuestion = require('../controller/controllerQuestion');
 
 // RUTAS
 router.get('/vehicle', vehicleController.getVehicles);
 
 router.get('/vehicle/:id', authenticateToken, vehicleController.getVehicleById);
-
 router.patch('/vehicle/:id', authenticateToken, vehicleController.updateVehicle);
 router.delete('/vehicle/:id', authenticateToken, vehicleController.deleteVehicle);
+
 router.post('/vehicle', upload.single('image'), vehicleController.createVehicle);
+
+router.post('/question/:vehicleId', authenticateToken, controllerQuestion.createQuestion);
+router.get('/question/:vehicleId', controllerQuestion.getQuestionsByVehicle);
+router.post('/answer/:questionId', authenticateToken, controllerAnswer.createAnswer);
+
 module.exports = router;
